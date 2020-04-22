72ae3c6cbc5d5eae88ed07206ce2b76cbe7104fe
Sangriale <senora.pelos@gmail.com>
2020-04-21 19:19:01 -0500

    work in schema file

--- schema.sql
+++ schema.sql
@@ -27,5 +27,20 @@ VALUE ("Ruby Red Grapefruit Juice", "Food", 7.05, 113);
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUE ("Goldfish Tumbler", "Kitchen", 19.99, 63);
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Harry Potter Tumbler", "Kitchen", 19.99, 37);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Birthda Massacre", "Music", 9.99, 129);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Bella Morte", "Music", 9.99, 211);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Back scratcher", "Novelties", 3.50, 13);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("The Child plush", "toys", 39.99, 43);
+
 
 
